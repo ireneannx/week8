0fcc1031156499ea410c9cc63064c568866a6bfb
ireneannx <irene2104@gmail.com>
2019-09-09 18:41:38 +0530

    added react context api to change theme of navbar

--- connect-backend/mern todo app/client/src/App.js
+++ connect-backend/mern todo app/client/src/App.js
@@ -7,19 +7,31 @@ import CreateTodo from './components/create-todo.component'
 import EditTodo from './components/edit-todo.component'
 import TodosList from './components/todos-list.component'
 import Navbar from './components/Navbar'
-
+//import Switch from './theme/Switch'
+import { ThemeProvider } from './theme/theme'
 
 class App extends Component {
+  state = {
+    theme: "light",
 
+    changeTheme: () => { //function inside state must follow render props
+      this.setState(({ theme }) => {
+        return (
+          { theme: theme == "light" ? "dark" : "light" })
+      })
+    }
+  }
   render() {
     return (
-      <div className="container">
-        <Navbar />
-
-        <Route exact path="/" component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Navbar />
+          <button onClick={this.state.changeTheme}> Change Theme </button>
+          <Route exact path="/" component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- connect-backend/mern todo app/client/src/components/Navbar.js
+++ connect-backend/mern todo app/client/src/components/Navbar.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeConsumer } from '../theme/theme'
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
-    <>
-      {/* the <> above is a react fragment. This removes the need for unnecessary div */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="" target="_blank">
-          {/* <img src={logo} width="30" height="30" /> */}
-        </a>
-        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-        <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">Todos</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/create" className="nav-link">Create Todo</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <br />
-    </>
+    <ThemeConsumer>
+      {({ theme }) => { ///use render props with context api 
+        var themestyle = theme == "light" ? "navbar-light bg-light" : "navbar-dark bg-dark"
+        return (
+          <nav className={`navbar navbar-expand-lg ${themestyle}`}>
+            <a class="navbar-brand" href="" target="_blank">
+              {/* <img src={logo} width="30" height="30" /> */}
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        )
+      }}
+    </ThemeConsumer>
   )
 }
 
